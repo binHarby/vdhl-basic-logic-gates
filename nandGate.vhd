149ff59aa14e23542deec3e8900c51702d53f741
binHarby <lance.alameri@gmail.com>
2022-12-04 11:36:50 +0400

    and Gate

---
+++ nandGate.vhd
@@ -0,0 +1,11 @@
+LIBRARY ieee;
+ USE ieee.std_logic_1164.all;
+ ENTITY nandGate IS
+ PORT( s0: IN STD_LOGIC;
+ s1: IN STD_LOGIC;
+ F0: OUT STD_LOGIC );
+ END nandGate;
+ ARCHITECTURE func OF nandGate IS
+ BEGIN
+ F0 <= s0 NAND s1;
+ END func;
