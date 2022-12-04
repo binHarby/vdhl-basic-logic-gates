6b6d839bac26ad5e3d1da508f3016c77a20ca092
binHarby <lance.alameri@gmail.com>
2022-12-04 11:37:39 +0400

    and Gate

---
+++ nor.vhd
@@ -0,0 +1,11 @@
+LIBRARY ieee;
+ USE ieee.std_logic_1164.all;
+ ENTITY norGate IS
+ PORT( s0: IN STD_LOGIC;
+ s1: IN STD_LOGIC;
+ F0: OUT STD_LOGIC );
+ END norGate;
+ ARCHITECTURE func OF norGate IS
+ BEGIN
+ F0 <= s0 NOR s1;
+ END func;
