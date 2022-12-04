f2df5d863b0ecbbb53c979337cccb2fa33488dca
binHarby <lance.alameri@gmail.com>
2022-12-04 11:35:35 +0400

    and Gate

---
+++ andGate.vhd
@@ -0,0 +1,11 @@
+ LIBRARY ieee;
+ USE ieee.std_logic_1164.all;
+ ENTITY andGate IS
+ PORT( s0: IN STD_LOGIC;
+ s1: IN STD_LOGIC;
+ F0: OUT STD_LOGIC );
+ END andGate;
+ ARCHITECTURE func OF andGate IS
+ BEGIN
+F0 <= s0 AND s1;
+END func;
---
+++ notGate.vhd
@@ -0,0 +1,10 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+ENTITY notGate IS
+PORT(s0: IN STD_LOGIC;
+F0: OUT STD_LOGIC);
+END notGate;
+ARCHITECTURE func OF notGate IS
+BEGIN
+F0 <= NOT s0;
+END func;
